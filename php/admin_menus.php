86f88898c61518b25123a2ef04432abc187de0ee
code4 <code4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-27 03:35:43 +0000

    blank files are added
    git-svn-id: https://plugins.svn.wordpress.org/timeline-graph/trunk@415753 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/admin_menus.php
@@ -0,0 +1,27 @@
+<div id="message" class="updated fade">
+	Time Line Graph Settings
+</div>
+<?php
+    if($_REQUEST)
+
+?>
+
+<div style="padding: 10px  0 40px 40px;">
+<form action="admin_menus.php" method="Get">
+    <table>
+        <tr>
+            <td>Data : </td>
+            <td><input type="file" name="data" /></td>
+        </tr>
+        <tr>
+            <td>Size : </td>
+            <td><input type="text" name="widthX" style="width: 89px;" /> X <input type="text" name="heightY" style="width: 87px;"/></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="submit" /></td>
+        </tr>
+    
+    </table>
+</form>
+</div>
